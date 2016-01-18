e47b383ab895407eb79929888cda5b0e708e60c3
Sean Richardson <Sean.r2000@hotmail.com>
2016-01-18 11:09:54 +0000

    added more stuff
    bugs need fixing

---
+++ Assets/Scripts/PhysicsObject.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhysicsObject : MonoBehaviour
+{
+    public bool useGravity = true;
+    public float massInGrains = 180;
+    public Vector2 muzzleVelocity = new Vector2(1000,0);// initial velocity in ft/s
+    public float muzzleEnergy; // ft-lbs
+    public Vector2 velocityOnImpact; // ft/s
+    public float density;
+    public float varyingBallisticDragCoefficient;
+    public float radius = 0.005f; // m
+    public float initialKineticEnergy;
+    public float finalKineticEnergy;
+
+    public float drag;
+
+    Rigidbody rb;
+    Simulation s;
+
+    public float massInKg; // kg
+    public Vector2 velocityFtPerSecond;
+    public Vector2 velocity; // m/s
+    public Vector2 netVelocity;
+    public Vector2 velocitySquared;
+
+    public Vector2 force;
+    public Vector2 acceleration;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        s = FindObjectOfType<Simulation>();
+
+        massInKg = massInGrains * 0.00006479891f;
+        rb.mass = massInKg;
+        velocityFtPerSecond = muzzleVelocity;
+        velocity = velocityFtPerSecond * 0.3048f;
+        muzzleEnergy = 0.5f * massInKg * (velocity.x * velocity.x);
+        muzzleEnergy = muzzleEnergy / 1.356f;
+        varyingBallisticDragCoefficient = 0.170f;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        //if (velocity != muzzleVelocity)
+        //{
+        //velocities.x = transform.position.x / Time.fixedDeltaTime;
+        //velocities.y = transform.position.y / Time.fixedDeltaTime;
+        //velocitySquared = velocities.x * velocities.x + velocities.y * velocities.y;
+        //velocity = Mathf.Sqrt(velocitySquared);
+        //velocityFtPerSecond = velocity / 0.3048f;
+        //}
+        velocity += acceleration * Time.fixedDeltaTime;
+        velocitySquared.x = velocity.x * velocity.x;
+        velocitySquared.y = velocity.y * velocity.y;
+
+        netVelocity.x = Mathf.Sqrt(velocitySquared.x);
+        netVelocity.y = Mathf.Sqrt(velocitySquared.y);
+
+        drag = s.densityOfAir * varyingBallisticDragCoefficient * Mathf.PI * radius * radius;
+        drag = drag / 2;
+
+        acceleration.x = (-drag / massInKg) * netVelocity.x * velocity.x;
+        acceleration.y = (-drag / massInKg) * netVelocity.y * velocity.y;
+
+        force = acceleration * massInKg;
+        rb.AddForce(force);
+    }
+}
---
+++ Assets/Scripts/Simulation.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Simulation : MonoBehaviour
+{
+    public float densityOfAir = 1.2922f;
+
+    public Vector2 projectileVelocity;
+    public float projectileAcceleration;
+    public float force;
+
+    public bool runSimulation;
+
+    public Collider projectile;
+    public Collider target;
+
+    public Vector3 projectileStartPosition;
+    public Vector3 projectilePosition;
+    public Vector3 targetPosition;
+
+    Rigidbody rb;
+    float mass;
+    PhysicsObject po;
+    // simulation needs
+    // fire a projectile
+    // instantiate the projectile and the target at a given distance to each other
+    // calculate the path of the projectile with a given mass and speed
+    // calculate the point of impact and how the material will deform
+    // play the simulation
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = projectile.GetComponent<Rigidbody>();
+        po = projectile.GetComponent<PhysicsObject>();
+        projectileStartPosition = projectile.transform.position;
+        projectilePosition = projectileStartPosition;
+        projectileVelocity = po.velocity;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey("space"))
+        {
+            runSimulation = true;
+        }
+        if(runSimulation)
+        {
+            rb.AddForce(Vector3.right * force * (Time.deltaTime / 1000), ForceMode.Impulse);
+            //projectilePosition.x += projectileVelocity * (Time.deltaTime/100);
+            //projectile.transform.position = projectilePosition;
+            runSimulation = false;
+        } 
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        //Debug.Log("collided");
+        if (col.gameObject.name == "Cube 1")
+            Debug.Log("collidied");
+    }
+}
